29aa8a12336848c165588ccae035f78298f40587
TheCodex6824 <thecodex6824@gmail.com>
2021-07-01 11:41:21 -0400

    Add missing license headers

--- src/main/java/thecodex6824/tcresearchpatcher/parser/ScanParserItemExtended.java
+++ src/main/java/thecodex6824/tcresearchpatcher/parser/ScanParserItemExtended.java
@@ -1,3 +1,23 @@
+/**
+ *  Thaumcraft Research Patcher
+ *  Copyright (c) 2021 TheCodex6824.
+ *
+ *  This file is part of Thaumcraft Research Patcher.
+ *
+ *  Thaumcraft Research Patcher is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Thaumcraft Research Patcher is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with Thaumcraft Research Patcher.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package thecodex6824.tcresearchpatcher.parser;
 
 import java.util.ArrayList;
